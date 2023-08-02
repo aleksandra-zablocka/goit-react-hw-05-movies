64c67ec92b256501540b46be9c8cadd48ef92b47
Aleksandra Zabłocka <aleksandra-zablocka@o2.pl>
2023-08-02 17:59:47 +0200

    changed input to form in movies

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,4 +1,3 @@
-// import { useSearchParams, Link, useLocation } from 'react-router-dom';
 import { useSearchParams, Link } from 'react-router-dom';
 import { fetchMovies } from 'api';
 import React, { useState, useEffect } from 'react';
@@ -7,38 +6,48 @@ import css from './Movies.module.css';
 export const Movies = () => {
   const [movies, setMovies] = useState([]);
   const [searchParams, setSearchParams] = useSearchParams();
+  const [searchQuery, setSearchQuery] = useState('');
 
-  console.log(searchParams)
-  
   useEffect(() => {
-    fetchMovies()
+    const title = searchParams.get('title') || ''; 
+    setSearchQuery(title);
+  }, [searchParams]);
+
+  useEffect(() => {
+    fetchMovies(searchQuery)
       .then(data => {
         setMovies(data.results);
       })
       .catch(error => {
         console.log('An error occurred', error);
       });
-  }, []);
+  }, [searchQuery]);
 
   const handleInput = title => {
-    const params = title !== '' ? { title } : {};
+    setSearchQuery(title);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const params = searchQuery !== '' ? { title: searchQuery } : {};
     setSearchParams(params);
   };
 
   const imageUrlBase = 'https://image.tmdb.org/t/p/';
   const imageSize = 'w500';
-  // const moviesWithTitles = movies.filter(movie => movie.title !== undefined);
 
   return (
     <div className={css.movieSearch}>
-      {/* <input className={css.inputSearch} type="text" value="" onChange={() => {}} placeholder="Search movies"/> */}
-      <input
-        className={css.inputSearch}
-        type="text"
-        value=""
-        onChange={e => handleInput(e.target.value)}
-        placeholder="Search movies"
-      />
+      <form onSubmit={handleSubmit}>
+        <input
+          className={css.inputSearch}
+          type="text"
+          value={searchQuery}
+          onChange={e => handleInput(e.target.value)}
+          placeholder="Search movies"
+        />
+        <button type="submit">Search</button>
+      </form>
       <div className={css.results}>Results</div>
       <div className={css.wrapper}>
         {movies.map(movie => (
@@ -58,55 +67,3 @@ export const Movies = () => {
     </div>
   );
 };
-
-// import { useSearchParams, Link, useLocation } from 'react-router-dom';
-// import React, { useState, useEffect } from 'react';
-// import { fetchMovies } from 'api';
-// import css from './Movies.module.css';
-
-// export const Movies = () => {
-//   const [movies, setMovies] = useState([]);
-//   const [searchParams, setSearchParams] = useSearchParams();
-//   const movieTitle = searchParams.get('title') ?? ""
-
-//   useEffect(() => {
-//     fetchMovies()
-//       .then(data => {
-//         setMovies(data.results);
-//       })
-//       .catch(error => {
-//         console.log('An error occurred', error);
-//       });
-//   }, []);
-
-//   const imageUrlBase = 'https://image.tmdb.org/t/p/';
-//   const imageSize = 'w500';
-
-// const handleInput = (title) => {
-//   const params = title !== "" ? {title} : {}
-//   setSearchParams(params)
-// }
-
-// const filteredMovies = movies.filter((movie) => {
-//   const movieTitleLowerCase = movieTitle.toLowerCase();
-//   return movie.title.toLowerCase().includes(movieTitleLowerCase)
-// })
-
-//   return (
-//     <div className={css.movieSearch}>
-//       <input className={css.inputSearch} type="text" value="" onChange={(e) => handleInput(e.target.value)} placeholder="Search movies"/>
-//       <div className={css.results}>Results</div>
-//       <div className={css.wrapper}>
-//         {filteredMovies.map(movie => (
-//           <Link to={`/movies/${movie.id}`} key={movie.id} className={css['movie-card']}>
-//             <img
-//               src={imageUrlBase + imageSize + movie.poster_path}
-//               alt="movie poster"
-//             />
-//             <div className={css['movie-title']}>{movie.title}</div>
-//           </Link>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
